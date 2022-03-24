6acb9c5c459cdd1256ea2ad1e235ba8d6ada4adc
Saj <101917549+sajsdev@users.noreply.github.com>
2022-03-24 12:37:10 +0700

    Update tracker.cpp
    added a feature so you don't have to recompile the file to add new users to scan/track list

--- linux/tracker.cpp
+++ linux/tracker.cpp
@@ -4,14 +4,20 @@
 #include <vector>
 #include <fstream>
 #include <ctime>
+#include <cctype>
+#include <algorithm>
 
 bool get_user_in_chat(const std::vector<std::string> &a, const std::string &b, std::vector<bool> &c);
+void add_user_in_check_list(std::vector<std::string> &check_list,const std::string &user);
+
 
 int main(){
     const std::string user{};                     //Change this value to twitch specific username you want to check their precense in check_list's chatroom (lowercase)
     const std::vector<std::string> check_list{};  //Change this value to target username's chatroom (lowercase)
     std::vector<bool> join_quit(check_list.size(), 0);
-
+    
+    add_user_in_check_list(check_list, user);
+    
     while (true)
     {
         if (!get_user_in_chat(check_list, user, join_quit)){
@@ -20,6 +26,7 @@ int main(){
     }
     return 0;
 }
+
 
 bool get_user_in_chat(const std::vector<std::string> &a, const std::string &b, std::vector<bool> &join_quit){
     
@@ -70,4 +77,59 @@ bool get_user_in_chat(const std::vector<std::string> &a, const std::string &b, s
     }
     out_file.close();
     return true;
+}
+
+
+void add_user_in_check_list(std::vector<std::string> &check_list,const std::string &user){
+    std::cin.clear();
+    char choice{'n'};
+    std::string new_user{};
+    std::cout << std::setw(70) << std::setfill('-') << "-" << std::endl;
+    std::cout << "You're currently scanning/tracking " << user << " in : \n";
+    std::cout << "\n" << std::setw(70) << std::setfill('-') << "-" << std::endl;
+    std::cout << std::setfill(' ');
+
+    for (size_t i = 0; i < check_list.size(); i++){
+        std::cout << std::setw(25) << std::left << check_list.at(i);
+        if ((i+1)%3 == 0){
+            std::cout << "\n" << std::setw(70) << std::setfill('-') << "-" << "\n" << std::endl;
+            std::cout << std::setfill(' ');
+        }
+    }
+    std::cout << "\n" << std::setw(70) << std::setfill('-') << "-" << "\n" << std::endl;
+    
+    std::cout << std::setfill(' ');
+    std::cout << std::endl;
+    std::cout << "Do you want to add a new user(s) ? (y/n) : ";
+    std::cin >> choice;
+    choice = std::tolower(choice);
+    
+    while(choice == 'y'){
+        size_t count{};
+        std::cout << "Enter how many user(s) you want to add and the users separated by space (for example: 4 sunshinebread jdcr 39daph erobb221) \n: ";
+        std::cin >> count;
+        
+        for (size_t i = 0; i < count; i++){
+            bool user_exist{false};
+            std::cin >> new_user;
+            std::for_each(new_user.begin(), new_user.end(), [](char &c){c = std::tolower(c);});
+            
+            for(const auto &u : check_list){
+                if (u == new_user){
+                    std::cout << new_user << " already exist in scan/track list." << std::endl;
+                    user_exist = true;
+                    break;
+                }
+            }
+
+            if (!user_exist){
+                check_list.push_back(new_user);
+            }
+        }
+        
+        choice = 'n';
+        add_user_in_check_list(check_list, user);
+    }
+
+    return;
 }
